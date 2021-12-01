a888c8c2338f854091ec380add30d2162aa56c1f
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2021-12-01 09:47:20 -0500

    Detection of fingers up and distance calculation :mag_right:

--- HandTracking/HandTrackingModule.py
+++ HandTracking/HandTrackingModule.py
@@ -1,21 +1,24 @@
 import cv2
+import math
 import time
 import mediapipe as mp
 
 
 class HandDetector:
     def __init__(self, mode=False, max_hands=2, detection_confidence=0.5, tracking_confidence=0.5):
-        self.multiHandLandMarks = None
+        self.landmark_list = None
+        self.multi_hand_landmarks = None
         self.mode = mode
-        self.maxHands = max_hands
-        self.detectionCon = detection_confidence
-        self.trackCon = tracking_confidence
+        self.max_hands = max_hands
+        self.detection_confidence = detection_confidence
+        self.tracking_confidence = tracking_confidence
 
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
-                                         min_detection_confidence=self.detectionCon,
-                                         min_tracking_confidence=self.trackCon)
+        self.hands = self.mp_hands.Hands(static_image_mode=self.mode, max_num_hands=self.max_hands,
+                                         min_detection_confidence=self.detection_confidence,
+                                         min_tracking_confidence=self.tracking_confidence)
         self.mp_drawing = mp.solutions.drawing_utils
+        self.tip_index = [4, 8, 12, 16, 20]
 
     def find_hands(self, image, draw=True):
         # Flip the image horizontally for later selfie-view display
@@ -35,10 +38,10 @@ class HandDetector:
         # Draw the hand annotations on the image
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        self.multiHandLandMarks = results.multi_hand_landmarks
+        self.multi_hand_landmarks = results.multi_hand_landmarks
 
-        if self.multiHandLandMarks:
-            for hand_Landmarks in self.multiHandLandMarks:
+        if self.multi_hand_landmarks:
+            for hand_Landmarks in self.multi_hand_landmarks:
                 if draw:
                     self.mp_drawing.draw_landmarks(image, hand_Landmarks, self.mp_hands.HAND_CONNECTIONS,
                                                    self.mp_drawing.DrawingSpec(color=(0, 220, 255), thickness=4,
@@ -49,20 +52,66 @@ class HandDetector:
         return image
 
     def find_position(self, image, hand_number=0, draw=True):
-        landmark_list = []
+        x_list = []
+        y_list = []
+        bbox = []
 
-        if self.multiHandLandMarks:
-            hand_landmarks = self.multiHandLandMarks[hand_number]
+        self.landmark_list = []
+        if self.multi_hand_landmarks:
+            hand_landmarks = self.multi_hand_landmarks[hand_number]
             for index, lm in enumerate(hand_landmarks.landmark):
                 # print(id, lm)
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                landmark_list.append([index, cx, cy])
+                x_list.append(cx)
+                y_list.append(cy)
+                # print(id, cx, cy)
+                self.landmark_list.append([index, cx, cy])
                 if draw:
                     if index in [4, 8, 12, 16, 20]:
-                        cv2.circle(image, (cx, cy), 6, (255, 0, 255), cv2.FILLED)
+                        cv2.circle(image, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
 
-        return landmark_list
+            x_minimum, x_maximum = min(x_list), max(x_list)
+            y_minimum, y_maximum = min(y_list), max(y_list)
+            bbox = x_minimum, y_minimum, x_maximum, y_maximum
+
+            if draw:
+                cv2.rectangle(image, (x_minimum - 20, y_minimum - 20), (x_maximum + 20, y_maximum + 20), (0, 255, 0), 2)
+
+        return self.landmark_list, bbox
+
+    def fingers_up(self):
+        finger_list = []
+        # Thumb
+        if self.landmark_list[self.tip_index[0][1]] > self.landmark_list[self.tip_index[0] - 1][1]:
+            finger_list.append(1)
+        else:
+            finger_list.append(0)
+
+        # Fingers
+        for index in range(1, 5):
+            if self.landmark_list[self.tip_index[index]][2] < self.landmark_list[self.tip_index[index] - 2][2]:
+                finger_list.append(1)
+            else:
+                finger_list.append(0)
+
+        # total_fingers = finger_list.count(1)
+
+        return finger_list
+
+    def find_distance(self, p1, p2, image, draw=True, r=15, t=3):
+        x1, y1 = self.landmark_list[p1][1:]
+        x2, y2 = self.landmark_list[p2][1:]
+        cx, cy = (x1 + x2) // (y1 + y2) // 2
+
+        if draw:
+            cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(image, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(image, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(image, (cx, cy), r, (255, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, image, [x1, x2, x2, y2, cx, cy]
 
 
 def main():
@@ -75,7 +124,9 @@ def main():
         start = time.time()
 
         image = detector.find_hands(image)
-        detector.find_position(image)
+        landmark_list, bbox = detector.find_position(image)
+        if len(landmark_list) != 0:
+            print(landmark_list[4])
 
         end = time.time()
         totalTime = end - start
